d55b36e83301c3ae21239c0298ad5eabbfaff7e7
Miran Ahmad <222miran222@gmail.com>
2025-02-26 01:57:39 +0530

    App complete

---
+++ constants.ts
@@ -0,0 +1,14 @@
+export const socials = [
+  {
+    id: 1,
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/mdmiranahmad/",
+    handle: "@mdmiranahamd",
+  },
+  {
+    id: 2,
+    name: "Instagram",
+    url: "https://www.instagram.com/mdmiranahmad/",
+    handle: "@mdmiranahamd",
+  },
+];
